12f8537817e2d4caffd0bbdd4056025cfa9a8d9c
djmenesesl <dmenesesl08@gmail.com>
2022-09-04 16:45:38 +0000

    Todolist con react realizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,69 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { object } from "prop-types";
+import React, { useState } from "react";
+import { FaRegTimesCircle } from "react-icons/fa";
 
 //create your first component
 const Home = () => {
+
+	const [newItem, setNewItem] = useState ("");
+	const [items, setItems] = useState([]);
+	const [count, setCount] = useState(0);
+
+	function addItem(){
+
+		if (!newItem){
+			alert("Enter an item.")
+			return;
+		}
+		
+	const item = {
+		id: Math.floor(Math.random() * 1000),
+		value: newItem
+		}
+
+		setItems(oldList => [...oldList, item]);
+		setNewItem("");
+
+	};
+
+	function deteleItem(id){
+		const newArray = items.filter(item => item.id !== id);
+		setItems(newArray);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			
+			<h1>todos</h1>
+			<input 
+			type="text"
+			placeholder="What needs to be done?"
+			value={newItem}
+			onChange={e => setNewItem(e.target.value)}
+			onKeyDown={ e => {
+				if (e.key == "Enter"){
+					addItem();
+				if(e.target.value)
+				{setCount(count + 1)}
+				}
+			}}  
+			/>
+
+			<ul>
+				{items.map(item => {
+					return (
+						<li key={item.id}>{item.value} 
+						<button className="boton" 
+						onClick={() => {
+							deteleItem(item.id); 
+							{setCount(count - 1)}
+						}
+						}>X</button></li>
+					)
+
+				})}
+			</ul>
+			<div className="contador">{count} item left </div>
 		</div>
 	);
 };
